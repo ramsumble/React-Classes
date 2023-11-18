c6cd49940398805186a05212917e9a3b7fe67e65
ramsumble <ram.sumble@gmail.com>
2023-11-18 13:06:19 +1100

    api call and state handle

--- pokemon-class/src/PokemonDisplay.jsx
+++ pokemon-class/src/PokemonDisplay.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React from "react"
 
-export default class PokemonDisplay {
-    constructor(props) {
+export default class PokemonDisplay extends React.Component{
+    constructor(props){
         super(props);
-
+        this.name = "Pikachu"
+        //state stores data per component
         this.state = {
             pokemonName: null
         }
     }
+    async componentDidMount(){
+        console.log("Did mount triggers after the component was rendered");
+        function getRandomPokemonId(){
+            return Math.floor(Math.random() * 1017) +1;
+        }
 
-    render() {
-        return(
-            <h2>Pokemon Name</h2>
-        )
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => {
+            throw new Error("API failure");
+        });
+    
+        if (response.status === "404") {
+            throw new Error("API did not have data for the requested ID")
+        };
+    
+        //Convert the response into usable JSON
+        let data = await response.json().catch(error => {
+            throw new Error("API did not return valid JSON");
+        })
+        console.log(data.name);
+        this.setState({pokemonName: data.name})
     }
+
+    componentWillUnmount(){
+        console.log("Will unmount triggers after the component is unmounted (unlikely in this app)")
+    }
+
+    render(){
+        if (this.state.pokemonName){
+            return(
+                <h2>{this.state.pokemonName}</h2>
+            )
+        } else {
+            return(
+                <h2>Loading...</h2>
+            )
+        }
+        
+    }
+    
 }
